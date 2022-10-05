6f54b34e0b236f1a76edac047e673e4637a78670
XiaoNaTongzhi <113130607+XiaoNaTongzhi@users.noreply.github.com>
2022-10-05 23:39:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@
 // int[] array = new int[10];
 // for (int i = 0; i < array.Length; i++)
 // {
-//     array[i] = new Random().Next(1, 10);
+//     array[i] = new Random().Next(-10, 10);
 // }
 // Console.WriteLine($"Числовой ряд: {String.Join(", ", array)};");
 
